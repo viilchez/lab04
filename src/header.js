ed375750af85cac60d7c15fc15baa0d844f6d3e5
viilchez <167554276+viilchez@users.noreply.github.com>
2024-04-19 12:19:03 -0500

    Add files via upload

---
+++ src/header.js
@@ -0,0 +1,7 @@
+const Header = () => {
+    return <header>
+        <p>header</p>
+    </header>;
+}
+
+export default Header;  
---
+++ src/index.js
@@ -0,0 +1,26 @@
+import React, { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import Message from "./message";
+import Header from "./header";
+import Main from "./main";
+import Aside from "./aside";
+import Footer from "./footer";
+
+
+const root = createRoot(document.getElementById("root"));
+root.render(
+    <StrictMode>
+        <Header></Header>
+        <div className="container">
+            <div className="row">
+                <div className="col-md-8">
+                    <Main></Main>
+                </div>
+                <div className="col-md-4">
+                    <Aside></Aside>
+                </div>
+            </div>
+        </div>
+        <Footer></Footer>
+    </StrictMode>
+);
